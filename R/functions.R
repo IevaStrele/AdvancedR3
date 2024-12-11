2406c5eb9484efbb93bfc4a8a0a3bd4858ef2895
Ieva Strele <ievastrele@inbox.lv>
2024-12-11 17:00:33 +0200

    the function of tidy logistic regression model added

--- R/functions.R
+++ R/functions.R
@@ -61,12 +61,58 @@ column_values_to_snake_case <- function(data, columns) {
 #' @return A data frame
 
 metabolites_to_wider <- function(data) {
-    data %>%
-        tidyr::pivot_wider(
-            names_from = metabolite,
-            values_from = value,
-            values_fn = mean,
-            names_prefix = "metabolite_"
-        )
+  data %>%
+    tidyr::pivot_wider(
+      names_from = metabolite,
+      values_from = value,
+      values_fn = mean,
+      names_prefix = "metabolite_"
+    )
 }
 
+
+
+#' A transformation recipe to pre-process the data
+#'
+#' @param data the lipidomics dataset
+#' @param metabolite_variable the column of the metabolites
+#'
+#' @return a data frame
+create_recipe_spec <- function(data, metabolite_variable) {
+  recipes::recipe(data) %>%
+    recipes::update_role(
+      {{ metabolite_variable }},
+      age,
+      gender,
+      new_role = "predictor"
+    ) %>%
+    recipes::update_role(class,
+      new_role = "outcome"
+    ) %>%
+    recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+}
+
+
+#' Create a workflow object of the model and transformations
+#'
+#' @param model_specs The model specs
+#' @param recipe_specs The recipe specs
+#'
+#' @return A workflow object
+create_model_workflow <- function(model_specs, recipe_specs) {
+  workflows::workflow() %>%
+    workflows::add_model(model_specs) %>%
+    workflows::add_recipe(recipe_specs)
+}
+
+
+#' Create a tidy output of model results
+#'
+#' @param workflow_fitted_model the model workflow object fitted
+#'
+#' @return A tidy dataframe
+tidy_model_output <- function(workflow_fitted_model) {
+  workflow_fitted_model %>%
+    workflows::extract_fit_parsnip() %>%
+    broom::tidy(exponentiate = TRUE)
+}
