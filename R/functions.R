fe3cbbe1f884052f2119dc7987742ad00dde36f5
Ieva Strele <ievastrele@inbox.lv>
2024-12-11 11:01:41 +0200

    function to make distribution plots of metabolites

--- R/functions.R
+++ R/functions.R
@@ -20,3 +20,18 @@ descriptive_stats <- function(data) {
     ))
   return(stats)
 }
+
+
+
+#' Distribution plots of metabolites
+#'
+#' @param lipidomics data
+#'
+#' @return A plot object.
+
+plot_distributions <- function(data) {
+    plot <- ggplot2::ggplot(data, ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+    return(plot)
+}
