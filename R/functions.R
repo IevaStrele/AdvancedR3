30de14aff72aedc67a0b217dcff55c5956dad69a
Ieva Strele <ievastrele@inbox.lv>
2024-12-11 15:19:04 +0200

    function to transform the metabolite column to wide format

--- R/functions.R
+++ R/functions.R
@@ -26,13 +26,47 @@ descriptive_stats <- function(data) {
 
 #' Distribution plots of metabolites
 #'
-#' @param lipidomics data
+#' @param data lipidomics data
 #'
 #' @return A plot object.
 
 plot_distributions <- function(data) {
-    plot <- ggplot2::ggplot(data, ggplot2::aes(x = value)) +
-        ggplot2::geom_histogram() +
-        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
-    return(plot)
+  plot <- ggplot2::ggplot(data, ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+  return(plot)
 }
+
+
+
+#' Convert a column's char values to snake case
+#'
+#' @param data The lipidomics dataset
+#' @param columns the column to convert
+#'
+#' @return A data frame
+column_values_to_snake_case <- function(data, columns) {
+  data %>%
+    dplyr::mutate(dplyr::across(
+      {{ columns }},
+      snakecase::to_snake_case
+    ))
+}
+
+
+#' Metabolite column to wider format
+#'
+#' @param data The lipidomics dataset
+#'
+#' @return A data frame
+
+metabolites_to_wider <- function(data) {
+    data %>%
+        tidyr::pivot_wider(
+            names_from = metabolite,
+            values_from = value,
+            values_fn = mean,
+            names_prefix = "metabolite_"
+        )
+}
+
